3a3eee4d4233c6d34bdfe68d09e2fd2b98868643
Omanhene20 <144060132+Omanhene20@users.noreply.github.com>
2025-04-11 23:44:54 -0500

    Create crypt_demo.c

---
+++ crypt_demo.c
@@ -0,0 +1,28 @@
+#define _XOPEN_SOURCE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <crypt.h>
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: <2 char salt> <8 char key>\n");
+        return -1;
+    }
+
+    struct crypt_data data;
+    data.initialized = 0;
+
+    char* salt = argv[1];
+    char* key = argv[2];
+    char* result;
+
+    result = crypt_r(key, salt, &data);
+    if (result == NULL) {
+        perror("crypt() failed");
+        return -1;
+    }
+
+    printf("Salt: %s\nPassword: %s\nResult: %s\n", salt, key, result);
+    return 0;
+}
